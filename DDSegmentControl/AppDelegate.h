6aca786cdc39c4e72edaab53a325062a06a71be7
alex1207cd <alex1207cd@163.com>
2015-12-28 14:43:23 +0800

    Initial Commit

---
+++ DDSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDSegmentControl
+//
+//  Created by Alex on 15/12/28.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDSegmentControl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DDSegmentControl
+//
+//  Created by Alex on 15/12/28.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
