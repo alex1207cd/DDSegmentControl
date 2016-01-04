c48477a41554d7f0a2f01f16979b586bb1db9f9b
alex1207cd <alex1207cd@163.com>
2016-01-04 17:06:30 +0800

    DDSegmentControl

---
+++ DDSegmentControl/DDSegmentControl.h
@@ -0,0 +1,13 @@
+//
+//  DDSegmentControl.h
+//  DDSegmentControl
+//
+//  Created by Alex on 15/12/29.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDSegmentControl : UIControl
+
+@end
---
+++ DDSegmentControl/DDSegmentControl.m
@@ -0,0 +1,13 @@
+//
+//  DDSegmentControl.m
+//  DDSegmentControl
+//
+//  Created by Alex on 15/12/29.
+//  Copyright © 2015年 Alex. All rights reserved.
+//
+
+#import "DDSegmentControl.h"
+
+@implementation DDSegmentControl
+
+@end
